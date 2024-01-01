0d95c79b2f1300419cc843802c6b7c0aab49c33f
Elkelany84 <drnet80@gmail.com>
2024-01-01 19:35:11 +0200

    EndVideo4

--- lib/view/screens/welcome_screen.dart
+++ lib/view/screens/welcome_screen.dart
@@ -1,13 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:waleed_asro_shopping_getx_api/utils/theme.dart';
+import 'package:waleed_asro_shopping_getx_api/view/widgets/text_utils.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("WelcomeScreen"),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.asset(
+                "assets/images/background.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              color: Colors.black.withOpacity(0.2),
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 250,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 190,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: TextUtils(
+                        text: "Welcome",
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        underLine: TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 60,
+                    width: 230,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextUtils(
+                          text: "ELGaml ",
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: mainColor,
+                          underLine: TextDecoration.none,
+                        ),
+                        TextUtils(
+                          text: "Shop",
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          underLine: TextDecoration.none,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 250,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                      backgroundColor: mainColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: TextUtils(
+                      text: "Get Start",
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      underLine: TextDecoration.none,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextUtils(
+                        text: "Don't Have an account? ",
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                        underLine: TextDecoration.none,
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: TextUtils(
+                            text: "SignUp",
+                            fontWeight: FontWeight.normal,
+                            fontSize: 18,
+                            color: Colors.white,
+                            underLine: TextDecoration.underline,
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

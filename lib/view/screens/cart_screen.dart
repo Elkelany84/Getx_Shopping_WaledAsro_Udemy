93bbef57b1d4668841ddb180b9b72ce6f02a654d
Elkelany84 <drnet80@gmail.com>
2024-01-20 12:13:45 +0200

    EndVideo28

--- lib/view/screens/cart_screen.dart
+++ lib/view/screens/cart_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:waleed_asro_shopping_getx_api/utils/theme.dart';
-import 'package:waleed_asro_shopping_getx_api/view/widgets/cart/empty_cart.dart';
+import 'package:waleed_asro_shopping_getx_api/view/widgets/cart/cart_product_card.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -18,7 +18,25 @@ class CartScreen extends StatelessWidget {
           backgroundColor: Get.isDarkMode ? darkGreyClr : mainColor,
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.backspace))],
         ),
-        body: EmptyCart(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 600,
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return CartProductCard();
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(
+                        height: 20,
+                      );
+                    },
+                    itemCount: 1),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/view/widgets/cart/cart_product_card.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:waleed_asro_shopping_getx_api/utils/theme.dart';
+
+class CartProductCard extends StatelessWidget {
+  const CartProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 5, left: 20, right: 20),
+      height: 130,
+      width: 100,
+      decoration: BoxDecoration(
+        color: Get.isDarkMode
+            ? pinkClr.withOpacity(0.7)
+            : mainColor.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 15),
+            height: 120,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                  image: NetworkImage(
+                    "https://unsplash.com/photos/a-neon-sign-is-lit-up-in-a-dark-room-8TELKvO6Zco",
+                  ),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            flex: 15,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "flutter developer code udemy flutter developer code udemy",
+                  style: TextStyle(
+                      overflow: TextOverflow.ellipsis,
+                      color: Get.isDarkMode ? Colors.white : Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "\$109.99",
+                  style: TextStyle(
+                      overflow: TextOverflow.ellipsis,
+                      color: Get.isDarkMode ? Colors.white : Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.remove_circle,
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
+                      )),
+                  Text(
+                    "1",
+                    style: TextStyle(
+                        overflow: TextOverflow.ellipsis,
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.add_circle,
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
+                      )),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  size: 20,
+                  color: Get.isDarkMode ? Colors.black : Colors.red,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
